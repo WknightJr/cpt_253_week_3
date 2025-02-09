56101496ff160982e80359fb7e974b6ef18991eb
WknightJr <austinbouchard6@gmail.com>
2025-02-09 17:53:07 -0500

    Update UseStateCounter.jsx

--- src/components/UseStateCounter.jsx
+++ src/components/UseStateCounter.jsx
@@ -1,14 +1,38 @@
-import * as React from 'react';
+import { useState } from 'react';
 
-export const UseStateCounter = () => {
+//Im not sure what needed to go here. This was my best guess//
+
+
+export const useStateCounter = () => {
+    const [count, setCount] = React.useState(0);
+
+//This is the counter the buttons use//
+
+
+function handleClick() {
+    setCount(count + 1);
+}
+
+function handleDecrease() {
+    setCount(count - 1);
+}
+
+function handleReset() {
+    setCount(0);
+}
+
+//These the the functions for the buttons. They will increase, decrease, or reset the count//
+
 
     return (
         <>
         <h2>useState counter example</h2>
-        <h1>0</h1>
-        <button>increase</button>
-        <button>decrease</button>
-        <button>reset</button>
+        <h1>{state.count}</h1>
+        <button onClick={handleClick}>increase</button>
+        <button onClick={handleDecrease}>decrease</button>
+        <button onClick={handleReset}>reset</button>
         </>
     )
 }
+
+//The functions have been added to the buttons//
